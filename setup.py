a667166de5838714138aa4e0cd183917ef21e870
sansbacon <eric@erictruett.com>
2021-09-02 08:58:21 -0500

    renamed to resultsdbrg

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
-# resultsdb/setup.py
+# resultsdbrg/setup.py
 # -*- coding: utf-8 -*-
 # Copyright (C) 2021 Eric Truett
 # Licensed under the MIT License
 
 from setuptools import setup, find_packages
 
-PACKAGE_NAME = "resultsdb"
+PACKAGE_NAME = "resultsdbrg"
 
 
 def run():
     setup(name=PACKAGE_NAME,
           version="0.1",
-          description="python library for analzying resultsdb",
+          description="python library for analzying resultsdbrg",
           author="Eric Truett",
           author_email="eric@erictruett.com",
           license="MIT",
           packages=find_packages(),
-          entry_points={'console_scripts': ['rdb=scripts.resultsdb:main']},
           package_data={PACKAGE_NAME: ["data/*.*"]},
           zip_safe=False,
           install_requires=["python-dateutil", "tzlocal", "requests"],
